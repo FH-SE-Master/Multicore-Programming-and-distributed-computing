b9821d33ec47881f26e83a112c347065cce91665
Thomas Herzog <S1610454013@fh-ooe.at>
2017-10-05 10:23:08 +0200

    - First steps with hello world program

---
+++ 1_hello_world/HelloWorld/HelloWorld/kernel.cu
@@ -0,0 +1,76 @@
+#include "cuda_runtime.h"
+#include "pfc_cuda_device_info.h"
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std::literals;
+
+// The kernel function
+__global__ void cs_kernel(int const text_size, char* const dp_destination, char* const dp_source)
+{
+	auto const i {blockIdx.x * blockDim.x + threadIdx.x};
+	
+	if(i < text_size) {
+		dp_destination[i] = dp_source[i];
+	}
+}
+
+int main()
+{
+	try
+	{
+		int count{0};
+		cudaGetDeviceCount(&count);
+
+		if (count > 0)
+		{
+			cudaSetDevice(0);
+			auto const device_info = pfc::cuda::get_device_info();
+			auto const device_props = pfc::cuda::get_device_props();
+
+			std::cout << "compute capability: " << device_info.cc_major << "." << device_info.cc_minor << std::endl;
+			std::cout << "Device: " << device_props.name << std::endl;
+
+			// Makes auto to intepret variable as a string type and not a character array
+			// Since C++ 11, availability to define literal type via suffix
+			// {} is a initializer
+			auto const text{"Hello world"s};
+			auto const text_size{std::size(text) + 1};
+			auto const tib{32}; // threads/block
+			auto const big{(text_size + tib - 1) / tib}; // block/grid
+			auto const* const hp_source{text.c_str()}; // points to start of string
+			auto* hp_destination{new char[text_size]{0}}; // points to device which is empty at initialization time
+
+			// Buffer on device
+			char* dp_source{nullptr};
+			char* dp_destination{ nullptr };
+			cudaMalloc(&dp_source, text_size);
+			cudaMalloc(&dp_destination, text_size);
+
+			// copy string host -> device, expensive operation
+			cudaMemcpy(dp_source, hp_source, text_size, cudaMemcpyHostToDevice);
+
+			// kernel call
+			cs_kernel <<<big,tib>>> (text_size, dp_destination, dp_source);
+
+			cudaDeviceSynchronize();
+			cudaGetLastError();
+
+			// copy result device -> host
+			cudaMemcpy(hp_destination, dp_destination, text_size, cudaMemcpyDeviceToHost);
+			std::cout << "result: " << hp_destination << std::endl;
+			
+			// free memory on host
+			delete[] hp_destination, hp_destination = nullptr;
+			// free memory on device
+			cudaFree(dp_source);
+			cudaFree(dp_destination);
+		}
+	}
+	catch (std::exception const& ex)
+	{
+		std::cerr << ex.what() << "\n";
+	}
+	return 0;
+}
